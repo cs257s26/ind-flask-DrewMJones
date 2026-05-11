d6beeb2d649e1e792a7bf2e3a7925a479d43cb6e
DrewMJones <jonesd2@carleton.edu>
2026-05-10 22:30:49 -0400

    semi final changes

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -1,1 +1,56 @@
-""
+
+DROP TABLE IF EXISTS birds;
+CREATE TABLE birds (
+  latitude real,
+  longitude real,
+  taxon_name real,
+  common_name real,
+  iconic_taxon real,
+  place_geuss real,
+  observer real,
+    );
+
+DROP TABLE IF EXISTS amphibians;
+CREATE TABLE birds (
+  latitude real,
+  longitude real,
+  taxon_name real,
+  common_name real,
+  iconic_taxon real,
+  place_geuss real,
+  observer real,
+    );
+
+DROP TABLE IF EXISTS reptiles;
+CREATE TABLE reptiles (
+  latitude real,
+  longitude real,
+  taxon_name real,
+  common_name real,
+  iconic_taxon real,
+  place_geuss real,
+  observer real,
+    );
+
+
+DROP TABLE IF EXISTS mammals;
+CREATE TABLE mammals (
+  latitude real,
+  longitude real,
+  taxon_name real,
+  common_name real,
+  iconic_taxon real,
+  place_geuss real,
+  observer real,
+    );
+
+DROP TABLE IF EXISTS insects;
+CREATE TABLE insects (
+  latitude real,
+  longitude real,
+  taxon_name real,
+  common_name real,
+  iconic_taxon real,
+  place_geuss real,
+  observer real,
+    );
